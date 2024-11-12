24c3ca7c168a0ed106a37c312565cae19df74843
chooyy <choo-yy@qq.com>
2024-11-12 10:48:58 +0800

    chore：添加配置文件，修改viewer参数

--- scripts/my_tum_endos.sh
+++ scripts/my_tum_endos.sh
@@ -4,6 +4,6 @@
     ./ORB-SLAM3/Vocabulary/ORBvoc.txt \
     ./cfg/ORB_SLAM3/Monocular/TUM/usb_cam.yaml \
     ./cfg/gaussian_mapper/Monocular/TUM/tum_mono.yaml \
-    ~/Downloads/datasets/TUM/endos_dataset\
+    ~/Downloads/datasets/TUM/endos_dataset3\
     ./results/tum_mono/endos \
 #    no_viewer
